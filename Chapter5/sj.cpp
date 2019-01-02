09aba7bdc48fbe9d4ce2e6c303ddcb2136253fdc
cildhdi <miles_yang@foxmail.com>
2019-01-02 10:52:56 +0800

    fix bugs

--- Chapter5/sj.cpp
+++ Chapter5/sj.cpp
@@ -33,17 +33,20 @@ int find(const String &s, const String &t)
 }
 
 //2
-void delete_string(char* str, unsigned int s, unsigned int m, unsigned int t)
+bool delete_string(char *str, int m, int t)
 {
+    int s = std::strlen(str);
+    if (m < s || t < (s - m))
+        return false;
     std::strncpy(str + s, str, m);
-    std::strncpy(str + m + t, str + s + m, s - m - t);
+    std::strncpy(str + s + m, str + m + t, s - m - t);
+    return true;
 }
-
 
 int main()
 {
 #define STR(arr) String(arr, sizeof(arr) / sizeof(char))
-    //return -1
+    //find_test78
     assert(find(STR(""), STR("")) == -1);
     assert(find(STR("abcdef"), STR("abcdef")) == -1);
     assert(find(STR(""), STR("aaaaaa")) == -1);
@@ -53,8 +56,17 @@ int main()
     assert(find(STR("abcdef"), STR("ab")) == 2);
     assert(find(STR("abcdef"), STR("abcd")) == 4);
     assert(find(STR("abcdef"), STR("abcde")) == 5);
-    assert(find(STR("aaaabc"), STR("ab")) == 5);
-
+    //std::cout << find(STR("abcdabcdabcd"), STR("bca"));
+    char s[100] = {0}, t[100] = {0};
+    std::cin >> s >> t;
+    std::cout << find(String(s, std::strlen(s)), String(t, std::strlen(t)));
+    //delete_string_test
+    char *ptr = new char[100];
+    std::memset(ptr, 0, 100 * sizeof(char));
+    std::strncpy(ptr, "abcdefghijklmnopqrstuvwxyz", 27);
+    delete_string(ptr, 10, 10);
+    std::cout << ptr;
+    delete[] ptr;
     system("pause");
     return 0;
 }
